27512a3769ce031b98bf38156d0dbfbaa9b19531
Dmitriy Fishman <fishman.code@gmail.com>
2023-03-02 18:09:40 +0200

    Created empty 'dashboard' and 'login' components

---
+++ src/components/dashboard.js
@@ -0,0 +1,8 @@
+
+const Dashboard = () => {
+  return(<div>
+    <p>Dashboard component</p>
+  </div>);
+}
+
+export default Dashboard;
---
+++ src/components/login.js
@@ -0,0 +1,8 @@
+
+const Login = () => {
+  return(<div>
+    <p>Login component</p>
+  </div>);
+}
+
+export default Login;
